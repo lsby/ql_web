77c40d0c13def5d078e3e1e6ec084e017fef681f
hbybyyang <2451759073@hotmail.com>
2020-04-22 19:37:00 +0800

    commit

--- tmp/page/index/事件.mjs
+++ tmp/page/index/事件.mjs
@@ -5,24 +5,25 @@ import 数据 from './数据.mjs'
 import { 提交 } from './全局.mjs'
 
 var 已连接后端 = true
-export var 后端事件 = {
-    connect: _ => {
-        后端事件.写日志('已连接到后端')
-        if (已连接后端 == false) return window.location = window.location
-    },
-    disconnect: _ => {
-        后端事件.写日志('后端已断开')
-        已连接后端 = false
-    },
-    写日志: str => 数据.日志 = 数据.日志
-        .split('\n')
-        .入队头(str)
-        .数组截取(0)(100)
-        .join('\n')
-        .trim(),
-    控制台: str => console.log(str),
+
+export var 后端事件 = {}
+后端事件.connect = _ => {
+    后端事件.写日志('已连接到后端')
+    if (已连接后端 == false) return window.location = window.location
 }
-export var 前端事件 = {
-    ws事件: _ => 提交('ws事件')('p-i-n-g'),
-    post事件: _ => $.post('/api/test', {}, data => console.log(data)),
+后端事件.disconnect = _ => {
+    后端事件.写日志('后端已断开')
+    已连接后端 = false
 }
+后端事件.写日志 = str => 数据.日志 = 数据.日志
+    .split('\n')
+    .入队头(str)
+    .数组截取(0)(100)
+    .join('\n')
+    .trim()
+后端事件.控制台 = str => console.log(str)
+
+export var 前端事件 = {}
+前端事件.ws事件 = _ => 提交('ws事件')('p-i-n-g')
+前端事件.post事件 = _ => $.post('/api/test', {}, data => console.log(data))
+前端事件.初始化 = _ => console.log('init')
