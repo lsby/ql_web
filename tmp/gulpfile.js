d52cb0a7eb70afb9f00d8a57c9fe6056d76d3e5d
hbybyyang <2451759073@hotmail.com>
2020-04-22 19:13:15 +0800

    commit

--- tmp/gulpfile.js
+++ tmp/gulpfile.js
@@ -1,7 +1,8 @@
 var nodemon = require('gulp-nodemon')
 var { watch } = require('gulp')
 
-exports.default = function defaultTask() {
+exports.default = function defaultTask(cb) {
     nodemon()
+    cb()
 }
 // watch(**/*.js, _=>_)
