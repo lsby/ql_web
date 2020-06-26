96b26389aca7129bfb7caedfa9be4228c0cce8e4
hbybyyang <2451759073@hotmail.com>
2020-06-27 03:05:34 +0800

    commit

--- tmp/api/route_api.js
+++ tmp/api/route_api.js
@@ -6,6 +6,9 @@ var requireContext = require('require-context')
 var 路由们 = getDir(requireContext(path.resolve(__dirname, './route/'), true, /.js$/))
 
 var router = express.Router()
-路由们.forEach(obj => router.post(`/${obj.name}`, 安全同步运行(obj.obj, (req, res, next) => next)))
+路由们.forEach(obj => router.post(`/${obj.name}`, 安全同步运行(obj.obj, (req, res, next) => err => {
+    console.error(err)
+    return next(err.toString())
+})))
 
 export default router
