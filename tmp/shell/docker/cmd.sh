dd4dcd9e7cb3293cf33d6bb5c3c80d426ad85347
hbybyyang <2451759073@hotmail.com>
2020-07-03 14:35:47 +0800

    commit

--- tmp/shell/docker/cmd.sh
+++ tmp/shell/docker/cmd.sh
@@ -1,3 +1,5 @@
 work_path=$(dirname $(readlink -f $0))
+npm run web_build
+npm run service_build
 npm run pm2_start
 npm run pm2_log
--- tmp/shell/docker/run.sh
+++ tmp/shell/docker/run.sh
@@ -10,7 +10,5 @@ bash ${work_path}/setMirror.sh
 echo '安装npm依赖'
 npm i
 
-echo '设置npm'
+echo '设置日志'
 npm run pm2_setlog
-npm run web_build
-npm run service_build
