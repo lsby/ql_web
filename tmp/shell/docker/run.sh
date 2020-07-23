9b1631f994226554ef16efc589e1a23065c00323
hbybyyang <2451759073@hotmail.com>
2020-07-23 17:16:13 +0800

    .

--- tmp/shell/docker/run.sh
+++ tmp/shell/docker/run.sh
@@ -8,7 +8,14 @@ bash ${work_path}/setMirror.sh
 # apt-get install libaio1 -y
 
 echo '安装npm依赖'
+# npm install -g cnpm --registry=https://registry.npm.taobao.org
+# cnpm i
+# cnpm i --verbose
+# cnpm i --loglevel silly
+
 npm i
+# npm install --verbose
+# npm install --loglevel silly
 
 echo '设置日志'
 npm run pm2_setlog
