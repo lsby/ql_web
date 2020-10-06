f28475287942e61aaaa9ed92f80d39f9a7d90d1a
hbybyyang <2451759073@qq.com>
2020-10-07 05:58:00 +0800

    debug

--- tmp/web/demo/多弹出框.js
+++ tmp/web/demo/多弹出框.js
@@ -7,7 +7,7 @@ function 弹框() {
         type: 1,
         shade: 0,
         title: '在线调试',
-        content: html(<h1>你好世界</h1>),
+        content: html`<h1>你好世界</h1>`,
         zIndex: layer.zIndex,
         success: function (layero) {
             layer.setTop(layero)
